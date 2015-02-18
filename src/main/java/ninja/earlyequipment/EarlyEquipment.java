1b723f5478d669fcd4c9ec48c66916a7fc904d49
ViolentNinjaD <fiddlybilly@gmail.com>
2015-02-18 08:38:28 +0000

    EventHandlers and Instances

--- src/main/java/ninja/earlyequipment/EarlyEquipment.java
+++ src/main/java/ninja/earlyequipment/EarlyEquipment.java
@@ -1,6 +1,10 @@
 package ninja.earlyequipment;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
 import static ninja.earlyequipment.EarlyEquipment.*;
 
 @Mod(modid = MOD_ID, name = MOD_NAME, version = MOD_VERSION)
@@ -9,4 +13,25 @@ public class EarlyEquipment
     public static final String MOD_ID = "earlyequipment";
     public static final String MOD_NAME = "EarlyEquipment";
     public static final String MOD_VERSION = "@VERSION@";
+
+    @Mod.Instance(MOD_ID)
+    public static EarlyEquipment instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        
+    }
 }
