e2ab557cac974b17c5aaefe9523a1b713e7bfe73
ViolentNinjaD <fiddlybilly@gmail.com>
2015-02-18 08:35:53 +0000

    Basic Mod Class

---
+++ src/main/java/ninja/earlyequipment/EarlyEquipment.java
@@ -0,0 +1,12 @@
+package ninja.earlyequipment;
+
+import cpw.mods.fml.common.Mod;
+import static ninja.earlyequipment.EarlyEquipment.*;
+
+@Mod(modid = MOD_ID, name = MOD_NAME, version = MOD_VERSION)
+public class EarlyEquipment 
+{
+    public static final String MOD_ID = "earlyequipment";
+    public static final String MOD_NAME = "EarlyEquipment";
+    public static final String MOD_VERSION = "@VERSION@";
+}
